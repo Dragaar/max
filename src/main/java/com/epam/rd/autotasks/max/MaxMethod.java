d323a0960795480142b746df78079bbdb2af30c0
Dragaar <rosivanyshyn@gmail.com>
2022-08-14 20:14:34 +0300

    111

--- src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
+++ src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
@@ -1,7 +1,14 @@
 package com.epam.rd.autotasks.max;
 
 public class MaxMethod {
+
     public static int max(int[] values) {
-        throw new UnsupportedOperationException();
+        int max = values[0];
+        for(int value : values)
+        {
+            if (value > max) max = value;
+        }
+        return max;
+        //throw new UnsupportedOperationException();
     }
 }
